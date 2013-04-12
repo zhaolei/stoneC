0c5eeacb3e286421f738cbd348a19ef0fe7cb70a
zhaolei03 <zhaolei03@baidu.com>
2013-04-12 16:59:39 +0800

    shell

---
+++ base/shell/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+
+#define MAX 20
+
+int main()
+{
+
+    char buf[20];
+    int status;
+    pid_t pid;
+    pid_t wpid;
+
+    while(fgets(buf, MAX, stdin)>0) {
+        pid = fork();
+        if(pid == 0) {
+            execlp(buf, buf, (char *)0);
+            printf("err: %s \n", strerror(errno));
+            exit(0);
+        }
+
+
+        wpid = wait(&status);
+        printf("wpid:%d  %d \n", wpid,status);
+    }
+
+    return 0;
+}
