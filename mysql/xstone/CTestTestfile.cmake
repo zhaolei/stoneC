214fef61bc0dcb1c21bbf6a4258f0fc03b6b0bbf
zhaolei03 <zhaolei03@baidu.com>
2013-08-14 16:59:54 +0800

    xstone mysql engine

---
+++ mysql/xstone/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /stone/ser/mysql-5.6.12/storage/xstone
+# Build directory: /stone/ser/mysql-5.6.12/storage/xstone
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
