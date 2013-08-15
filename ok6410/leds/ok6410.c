249a746f7af97cfa243c25f3f8b822aff199a28a
zhaolei03 <zhaolei03@baidu.com>
2013-08-15 18:44:39 +0800

    led ok

--- ok6410/leds/ok6410.c
+++ ok6410/leds/ok6410.c
@@ -3,75 +3,30 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdlib.h>
-int led_disp(int argc, char *argv[])
+
+int main(int argc,char *argv[])
 {
+
     int fd;
-    char file_path[30];
-    memset(file_path,30,0);
-       int led=argv[1][0]-'0';
-       int sw=argv[2][0]-'0';
-     
-     
-    
-    
-    fd=open("/dev/leds",O_RDWR);
-    
-     ioctl(fd,sw,led);
-    if(fd < 0)
-    {
-        printf("Open %s Device Faild!\n",file_path);
-        exit(1);
+    int sw,led;
+
+    if(argc < 2) {
+        printf("Usage ledx command lednum \r\n"); 
+        return;
     }
-    else
-    	{
-    	printf("open %s ok\n",file_path)	;
-    	}
+
+    sw = atoi(argv[1]);
+    led = atoi(argv[2]);
+
+    printf("sw: %d, led:%d \r\n", sw, led);
+    fd=open("/dev/leds",O_RDWR);
+
 
     
-    
-      //  write(fd,argv[2],1);       
-	printf("write %c to %s using fd=%d\n",argv[2][0],file_path,fd);
-	sleep(3);
-
-
+    ioctl(fd,sw,led);
     close(fd);
 
     return 0;
 }
-int main(int argn,char * argv[])
-
-{
-
-  printf("Content-Type: text/html\n\n");
-
-  
-   printf("<html>\n");
-
-    printf("<head><title>OK6410 Test </title></head>\n");
-
-
-    printf("<body><pre>\n");
-
-
-  
-
-      while(argn-->0)  
-
-      {
-
-       printf("arg[%d]=%s",argn,argv[argn]);
-
-
-        }//注意<br \>，这个是网页上的换行，因为C语言中\n是不能在网页上起到换行作用的
-      led_disp(argn,argv);
-	 printf("</body>\n");
-
-
-	  printf("</html>\n");
-
-
-	    return 0;
-
-	    }
 
 
